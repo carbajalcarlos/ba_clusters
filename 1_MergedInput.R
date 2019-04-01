5a823321c826b13bd87112a49f997efddefa2168
Carlos Carbajal <carbajal.carlos@hotmail.com>
2019-04-01 17:23:13 +0100

    CODING: Separation of the main script and the analysis cluster script

--- 1_MergedInput.R
+++ 1_MergedInput.R
@@ -1,7 +1,7 @@
 # ===== Loading required libraries =====
 require(bibliometrix)
+require(quanteda)
 require(stringdist)
-require(openxlsx)
 require(tm)
 
 # ===== Manual reading of the codes =====
@@ -26,9 +26,12 @@ for (i in 1:length(files)) {
 raw <- unlist(raw)
 bm.wos <- convert2df(file = raw,dbsource = "isi", format = "bibtex")
 
+
+
 # ===== Merging dataframes =====
 ## Combining dataframes
-bm <- mergeDbSources(bm.sco, bm.wos, remove.duplicated = TRUE)
+#bm <- mergeDbSources(bm.sco, bm.wos, remove.duplicated = TRUE)
+bm <- bm.wos
 ## Cleaning merged dataframe
 bm <- subset(x = bm, subset = PY < 2019)
 bm <- subset(x = bm, subset = DT == "ARTICLE" | DT == "CONFERENCE PAPER" | DT == "PROCEEDINGS PAPER")
@@ -102,11 +105,11 @@ rm(bm.sco)
 rm(bm.wos)
 
 # Storing data files
-directory <- gsub(pattern = ".*\\\\([[:alnum:]]+)$", replacement = "\\1", x = directory)
-save.image(file = paste(c("1_process/", directory, "_image.RData"), collapse = ""))
-save(list = "bm", file = paste(c("1_process/", directory, "_bm.RData"), collapse = ""))
-write.xlsx(x = bm, file = paste(c("1_process/", directory, "_bm.xlsx"), collapse = ""),
-           sheetName="Sheet1", col.names=TRUE, row.names=TRUE, append=FALSE, showNA=FALSE)
+output <- gsub(pattern = ".*\\\\([[:alnum:]]+)$", replacement = "\\1", x = directory)
+save.image(file = paste(c("1_process/", output, "_image.RData"), collapse = ""))
+save(list = "bm", file = paste(c("1_process/", output, "_bm.RData"), collapse = ""))
+# write.xlsx(x = bm, file = paste(c("1_process/", directory, "_bm.xlsx"), collapse = ""),
+#            sheetName="Sheet1", col.names=TRUE, row.names=TRUE, append=FALSE, showNA=FALSE)
 
 # ===== Adopting strategies =====
 M <- bm
@@ -167,66 +170,13 @@ M$ID[index] <- trimws(gsub(pattern = "[^;]*INTERNET OF THING[^;]*", ignore.case 
 # lista <- trimws(unlist(strsplit(na.omit(M$ID), split = ";")), which = "both")
 # lista <- data.frame(table(lista))
 
-# ===== Text mining cluster formation =====
-require(quanteda)
-
-# Adding identifier to the dataset
-M$doc_id <-  paste("ipdm", 1:(nrow(M)), sep = "_")
-# Composing corpus
-# corp_bm <- corpus(M$AB_RAW, docnames = M$doc_id)
-corpus_raw <- corpus(M$TI_TM, docnames = M$doc_id)
-docvars(corpus_raw, "author") <- M$AU
-docvars(corpus_raw, "paper_numeric") <- seq_len(ndoc(corpus_raw))
-
-# # Testing the composition
-# summary(corpus_raw)
-# texts(corpus_raw)[10] %>% 
-#   stringi::stri_sub(1, 240) %>% 
-#   cat()
-
-dfm_prep <- dfm(corpus_raw, remove_numbers = TRUE, tolower = TRUE,
-                remove_punct = TRUE, verbose = TRUE)
-# dfm_papers <- dfm(dfm_prep, stem = TRUE, remove = stopwords("english"))
-dfm_papers <-  dfm(x = dfm_prep)
-dfm_papers <- dfm_papers[, order(featnames(dfm_papers))]
-#head(dfm_papers, nf = 10)
-
-## Removing unwanted terms from the dataframe.
-dict <- dictionary(list(blackholes = c("industri", "model", "busi")))
-
-dfm_papers <- dfm_remove(x = dfm_papers, pattern = dict)
-#head(dfm_papers, nf = 10)
-
-# textplot_wordcloud()  plot word clouds of the most frequent words in Papers 12 and 24
-#set.seed(15)
-#textplot_wordcloud(dfm_papers[c("ipdm_12", "ipdm_24"), ], 
-#                   max_words = 50, comparison = TRUE)
-
-# identify clusters of similar essay based on term frequency-inverse document frequency 
-dfm_papers_tfidf <- dfm_tfidf(dfm_papers, base = 10)
-# these are the 10 most important words from a specific article
-#topfeatures(dfm_papers_tfidf[50, ], n = 10)
-
-k <- 4
-
-km_out <- stats::kmeans(dfm_papers_tfidf, centers = k, algorithm = )
-km_out$iter
-
-colnames(km_out$centers) <- featnames(dfm_papers_tfidf)
-
-for (i in 1:k) { # loop for each cluster
-  cat("CLUSTER", i, "\n")
-  cat("Top 10 words:\n") # 10 most important terms at the centroid
-  print(head(sort(km_out$centers[i, ], decreasing = TRUE), n = 10))
-  cat("\n")
-  cat("Publications classified: \n") # extract essays classified
-  print(docnames(dfm_papers_tfidf)[km_out$cluster == i])
-  cat("\n")
-}
+# ===== DO TO M ====
+index <- grep(pattern = "business|management", x = M$SC, ignore.case = TRUE)
+M <- M[index, ]
 
 
 # Storing M into RData file 
-save(list = "M", file = paste(c("1_process/", directory, "_M.RData"), collapse = ""))
+save(list = "M", file = paste(c("1_process/", output, "_M2.RData"), collapse = ""))
 
 #Working 
 if (FALSE) {
